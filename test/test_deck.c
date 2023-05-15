e679942e5dd5b3b8375f67a75458f81e437392e7
Elliot C. Edwards <elliot.c.edwards@gmail.com>
2023-05-15 14:46:16 -0400

    Move test suite to libtap

--- test/test_deck.c
+++ test/test_deck.c
@@ -1,15 +1,10 @@
-#include "test.h"
-#include <cards.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include "../src/cards.h"
+#include "tap.h"
 
 int
 main(void)
 {
-        struct CARDS_deck_s *D =
-            CARDS_deck_init(CARDS_standard_deck, CARDS_standard_deck_size);
-        ok_hard_error(D);
-
-        CARDS_deck_destroy(D);
-        return 0;
+        plan(2);
+        ok(true);
+        done_testing();
 }
